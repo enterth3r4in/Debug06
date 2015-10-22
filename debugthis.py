ef2644b3a683705e80f47ec228848cf8ab1e493c
enterth3r4in <believe.the.rain@gmail.com>
2015-10-22 13:34:36 -0400

    Simple debug assignment

--- debugthis.py
+++ debugthis.py
@@ -1,5 +1,35 @@
-year = 0
+﻿year = 0
 cans = 0
 SIZE = 4
 QUIT = 9
 collected = [0, 0, 0, 0]
+HEADER1 = "Can Recycling Report"
+HEADER2 = "Year   Cans Collected"
+
+def isInt(value):
+    try:
+        int(value)
+        return True
+    except:
+        return False
+
+year = input("Enter year of student (1-4) or 'QUIT' to quit: ")
+if(isInt(year)):
+    while not(str(year) == "QUIT"):
+        cans = int(input("Enter number of cans collected: "))
+        collected[int(year) - 1] += cans
+        year = input("Enter year of student (1-4) or 'QUIT' to quit: ")
+        if(isInt(year)):
+            continue
+        else:
+            print("Must enter integer value")
+
+print()
+print(HEADER1)
+print(HEADER2)
+year = 0
+while (year < SIZE):
+    print(year + 1, "     ", collected[year])
+    year += 1
+
+
